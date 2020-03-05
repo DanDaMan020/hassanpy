25f7b79fc797cc443648d99603049266c1864d43
DanDaMan020 <dandaman020@gmail.com>
2020-03-04 22:07:04 -0600

    created bot structure, commands

--- mainbot.py
+++ mainbot.py
@@ -1,25 +1,31 @@
 # bot.py
 import os
 
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-bot = discord.Bot()
+bot = commands.Bot(command_prefix='')
+
+
+@bot.command(name='gg')
+async def on_gg(ctx):
+    await ctx.send('gg')
+
+
+@bot.command(name='wtf')
+async def on_wtf(ctx):
+    await ctx.send('wtf')
 
 
 @bot.event
-async def on_message(message):
+async def on_bog_event(message):
     lowermessage = message.lower()
-    if message.author == bot.user:
-        return
-    if lowermessage.content == 'gg':
-        response = 'gg'
-        await message.channel.send(response)
-    elif message.content == 'wtf':
-        response = 'wtf'
-        await message.channel.send(response)
+    allbogs = ('bog', 'pog', 'poggers', 'b o g')
+    for bog in allbogs:
+        if bog == lowermessage:
+            await message.channel.send('b o g')
 
 bot.run(token)
